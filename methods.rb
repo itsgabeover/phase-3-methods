41879fe4a828ed2d23aceacbe5353cba418406e4
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-09-20 11:06:14 -0700

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet aName
+    puts "Hello, #{aName}!"
+end
+
+def greet_with_default aName = "programmer"
+    puts "Hello, #{aName}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve anInteger
+    if anInteger.class != Integer
+        return nil
+    end
+    anInteger/2
+end
+
